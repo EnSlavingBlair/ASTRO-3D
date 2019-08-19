da347cd5e16e5363034c1966f4c1bd112778dce5
Blair <44489920+EnSlavingBlair@users.noreply.github.com>
2019-08-19 17:21:42 +0800

    GUI input of number of rolls
    Changed so that terminal is not what the new number is entered into, instead is a tkinter GUI.

--- addData.py
+++ addData.py
@@ -1,10 +1,35 @@
-# keep asking the user for input until the user inputs "end" (without the quotation marks)
-# writes input data to file
-running = True
-while running:
-    user_input = input("How many dice rolls to escape the Cosmic Microwave Background?")
-    if user_input == "end":
-        running = False
-    if(user_input.isdigit()):
-        with open("histData.txt", "a") as f:
-            f.write(str(user_input)+"\n")
+import tkinter as tk
+
+def save_entry_fields():
+    global num
+    num = e1.get()
+    print("Number: %s" % (e1.get()))
+    with open("histData.txt", "a") as f:
+        if(num.isdigit()):
+            f.write(str(num)+"\n")
+        else:
+            print("Not a valid number")
+
+master = tk.Tk()
+
+tk.Label(master, 
+         text="Number of rolls:  ", 
+         font = ("Times New Roman", 20)).grid(row=0, column=0)
+
+e1 = tk.Entry(master, 
+              font = ("Times New Roman", 16), 
+              bd = 5, 
+              width = 5)
+
+e1.grid(row=0, column=1)
+
+tk.Label(master,
+        text = "  ",
+        font = ("Times New Roman", 20)).grid(row=0, column=2)
+
+tk.Button(master, 
+          text='Save', 
+          command=save_entry_fields, 
+          font = ("Times New Roman", 16)).grid(row=0, column=3, sticky=tk.W, pady=4)
+
+tk.mainloop()
